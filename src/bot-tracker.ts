81659ac6aeddc409955f16a2f9bc816729cbc3c8
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-02-10 23:26:40 +0400

    Add saving applications

--- src/bot-tracker.ts
+++ src/bot-tracker.ts
@@ -1,5 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api'
 import { getApplicationStatus } from './status'
+import path from 'path'
+import fs from 'fs/promises'
 
 const TELEGRAM_BOT_TRACKING_API_KEY = process.env.TELEGRAM_BOT_TRACKING_API_KEY
 if (!TELEGRAM_BOT_TRACKING_API_KEY) {
@@ -14,8 +16,74 @@ const userStates = new Map<
   number,
   | { state: 'input_reference_number' }
   | { state: 'input_date_of_birth'; referenceNumber: string }
+  | {
+      state: 'input_application_name'
+      referenceNumber: string
+      dateOfBirth: Date
+    }
   | { state: 'loading'; editMessageId: number | null }
 >()
+
+const userDb = new Map<
+  number,
+  {
+    savedApplications: {
+      referenceNumber: string
+      dateOfBirth: Date
+      name: string
+    }[]
+  }
+>()
+
+async function parseDb() {
+  let dbFile: string
+  try {
+    dbFile = await fs.readFile(path.join(__dirname, '../db.txt'), 'utf-8')
+  } catch {
+    dbFile = ''
+  }
+  const lines = dbFile.split('\n')
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    const del1 = line.indexOf('\t')
+    const del2 = line.indexOf('\t', del1 + 1)
+    const del3 = line.indexOf('\t', del2 + 1)
+    const id = Number(line.substring(0, del1))
+    const referenceNumber = line.substring(del1 + 1, del2)
+    const dateOfBirth = new Date(line.substring(del2 + 1, del3))
+    const name = line.substring(del3 + 1)
+    if (!userDb.has(id)) {
+      userDb.set(id, { savedApplications: [] })
+    }
+    userDb
+      .get(id)!
+      .savedApplications.push({ referenceNumber, dateOfBirth, name })
+  }
+}
+
+parseDb()
+
+let saveFileLock = false
+async function saveDb() {
+  if (saveFileLock) return
+  saveFileLock = true
+  let dbFile = ''
+  for (const [id, user] of userDb) {
+    for (const application of user.savedApplications) {
+      dbFile +=
+        id +
+        '\t' +
+        application.referenceNumber +
+        '\t' +
+        application.dateOfBirth.toISOString() +
+        '\t' +
+        application.name +
+        '\n'
+    }
+  }
+  await fs.writeFile(path.join(__dirname, '../db.txt'), dbFile)
+  saveFileLock = false
+}
 
 type Scene =
   | 'mainMenu'
@@ -24,8 +92,11 @@ type Scene =
   | 'incorrectReferenceNumber'
   | 'inputDateOfBirth'
   | 'incorrectDateOfBirth'
+  | 'inputApplicationName'
+  | 'incorrectApplicationName'
   | 'fetchingApplicationStatus'
   | 'errorWhileFetchingApplicationStatus'
+  | 'applicationMenu'
 
 type SceneConfig = {
   text: string
@@ -132,13 +203,66 @@ const scenes: Record<Scene, SceneConfig> = {
       ],
     },
   },
+  inputApplicationName: {
+    text: 'Чтобы вам было удобнее ориентироваться в меню бота, введите название для заявки (например, Ваше имя). Макс. 30 символов',
+    replyMarkup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Назад',
+            callback_data: 'back_to_birthday_date',
+          },
+        ],
+        [
+          {
+            text: 'Отмена',
+            callback_data: 'main',
+          },
+        ],
+      ],
+    },
+  },
+  incorrectApplicationName: {
+    text: 'Некорректное имя для заявки. Макс. 30 символов. Попробуйте еще раз',
+    replyMarkup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Назад',
+            callback_data: 'back_to_birthday_date',
+          },
+        ],
+        [
+          {
+            text: 'Отмена',
+            callback_data: 'main',
+          },
+        ],
+      ],
+    },
+  },
   fetchingApplicationStatus: {
     text: 'Получение статуса заявки... (это может занять до 30 секунд)',
   },
   errorWhileFetchingApplicationStatus: {
     text: 'Не удалось получить статус заявки. Попробуйте позже',
   },
+  applicationMenu: {
+    text: 'Выберите действие',
+    replyMarkup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Назад',
+            callback_data: 'main',
+          },
+        ],
+      ],
+    },
+  },
 }
+
+const referenceNumberRegex = /^EVN\d+$/
 
 bot.on('message', async (msg) => {
   if (msg.chat.type !== 'private' || !msg.from || msg.from.is_bot) {
@@ -149,8 +273,7 @@ bot.on('message', async (msg) => {
   if (userState) {
     if (userState.state === 'input_reference_number') {
       if (msg.text) {
-        const referenceNumberRegex = /^EVN\d+$/
-        if (referenceNumberRegex.test(msg.text)) {
+        if (referenceNumberRegex.test(msg.text) && msg.text.length < 30) {
           userStates.set(msg.from.id, {
             state: 'input_date_of_birth',
             referenceNumber: msg.text,
@@ -168,19 +291,6 @@ bot.on('message', async (msg) => {
         const dateOfBirthRegex = /^(\d{2})\.(\d{2})\.(\d{4})$/
         if (dateOfBirthRegex.test(msg.text)) {
           const [, day, month, year] = msg.text.match(dateOfBirthRegex)!
-          userStates.set(msg.from.id, {
-            state: 'loading',
-            editMessageId: null,
-          })
-          const messageId = await goToScene(
-            msg,
-            scenes.fetchingApplicationStatus,
-            true,
-          )
-          userStates.set(msg.from.id, {
-            state: 'loading',
-            editMessageId: messageId,
-          })
           const birthdayDate = new Date(
             parseInt(year, 10),
             parseInt(month, 10) - 1,
@@ -190,8 +300,8 @@ bot.on('message', async (msg) => {
             0,
             0,
           )
-          fetchApplicationStatus({
-            telegramUserId: msg.from.id,
+          userStates.set(msg.from.id, {
+            state: 'input_application_name',
             referenceNumber: userState.referenceNumber,
             dateOfBirth: birthdayDate,
           })
@@ -202,6 +312,30 @@ bot.on('message', async (msg) => {
         await goToScene(msg, scenes.incorrectDateOfBirth, true)
       }
       return
+    } else if (userState.state === 'input_application_name') {
+      if (msg.text && msg.text.length <= 30) {
+        userStates.set(msg.from.id, {
+          state: 'loading',
+          editMessageId: null,
+        })
+        const messageId = await goToScene(
+          msg,
+          scenes.fetchingApplicationStatus,
+          true,
+        )
+        userStates.set(msg.from.id, {
+          state: 'loading',
+          editMessageId: messageId,
+        })
+        fetchApplicationStatus({
+          name: msg.text,
+          telegramUserId: msg.from.id,
+          referenceNumber: userState.referenceNumber,
+          dateOfBirth: userState.dateOfBirth,
+        })
+      } else {
+        await goToScene(msg, scenes.incorrectApplicationName, true)
+      }
     } else if (userState.state === 'loading') {
       userStates.set(msg.from.id, {
         state: 'loading',
@@ -211,21 +345,43 @@ bot.on('message', async (msg) => {
     }
   }
 
+  const savedApplications = getSavedApplications(msg.from.id)
   bot.sendMessage(msg.chat.id, scenes.mainMenu.text, {
-    reply_markup: scenes.mainMenu.replyMarkup,
+    reply_markup: {
+      inline_keyboard: [
+        ...savedApplications.map((a) => [
+          {
+            text: a.name + ' (' + a.referenceNumber + ')',
+            callback_data: 'application_' + a.referenceNumber,
+          },
+        ]),
+        ...(scenes.mainMenu.replyMarkup?.inline_keyboard ?? []),
+      ],
+    },
   })
 })
+
+function getSavedApplications(telegramUserId: number) {
+  return userDb.get(telegramUserId)?.savedApplications ?? []
+}
 
 async function goToScene(
   replyTo: TelegramBot.Message,
   scene: SceneConfig,
   forceNewMessage = false,
+  additionalButtons?: TelegramBot.InlineKeyboardButton[][],
 ) {
   if (!replyTo.from) return null
+  const replyMarkup: TelegramBot.InlineKeyboardMarkup = {
+    inline_keyboard: [
+      ...(scene.replyMarkup?.inline_keyboard ?? []),
+      ...(additionalButtons ? additionalButtons : []),
+    ],
+  }
   if (forceNewMessage) {
     try {
       const msg = await bot.sendMessage(replyTo.from.id, scene.text, {
-        reply_markup: scene.replyMarkup,
+        reply_markup: replyMarkup,
       })
       return msg.message_id
     } catch (e) {
@@ -237,13 +393,13 @@ async function goToScene(
       await bot.editMessageText(scene.text, {
         message_id: replyTo.message_id,
         chat_id: replyTo.chat.id,
-        reply_markup: scene.replyMarkup,
+        reply_markup: replyMarkup,
       })
       return replyTo.message_id
     } catch {
       try {
         const msg = await bot.sendMessage(replyTo.from.id, scene.text, {
-          reply_markup: scene.replyMarkup,
+          reply_markup: replyMarkup,
         })
         return msg.message_id
       } catch (e) {
@@ -258,6 +414,9 @@ bot.on('callback_query', async (query) => {
   bot.answerCallbackQuery(query.id)
   if (!query.message) return
 
+  const userState = userStates.get(query.from.id)
+  if (userState && userState.state === 'loading') return
+
   switch (query.data) {
     case 'main':
       userStates.delete(query.from.id)
@@ -271,16 +430,98 @@ bot.on('callback_query', async (query) => {
       userStates.delete(query.from.id)
       await goToScene(query.message, scenes.about)
       break
-    default:
+    case 'back_to_birthday_date': {
+      if (!userState || userState.state !== 'input_application_name') {
+        break
+      }
+      userStates.set(query.from.id, {
+        state: 'input_date_of_birth',
+        referenceNumber: userState?.referenceNumber ?? '',
+      })
+      await goToScene(query.message, scenes.inputDateOfBirth)
       break
+    }
+    case undefined:
+      break
+    default: {
+      if (
+        query.data.startsWith('application_') ||
+        query.data.startsWith('status_') ||
+        query.data.startsWith('delete_')
+      ) {
+        const referenceNumber = query.data.substring('status_'.length)
+        if (!referenceNumberRegex.test(referenceNumber)) {
+          break
+        }
+        const savedApplications = getSavedApplications(query.from.id)
+        const savedApplication = savedApplications.find(
+          (a) => a.referenceNumber === referenceNumber,
+        )
+        if (!savedApplication) {
+          break
+        }
+        if (query.data.startsWith('application_')) {
+          await goToScene(query.message, scenes.applicationMenu, false, [
+            [
+              {
+                text: 'Проверить статус заявки',
+                callback_data: 'status_' + referenceNumber,
+              },
+            ],
+            [
+              {
+                text: 'Удалить',
+                callback_data: 'delete_' + referenceNumber,
+              },
+            ],
+          ])
+        } else if (query.data.startsWith('status_')) {
+          userStates.set(query.from.id, {
+            state: 'loading',
+            editMessageId: null,
+          })
+          const messageId = await goToScene(
+            query.message,
+            scenes.fetchingApplicationStatus,
+            true,
+          )
+          userStates.set(query.from.id, {
+            state: 'loading',
+            editMessageId: messageId,
+          })
+          fetchApplicationStatus({
+            name: savedApplication.name,
+            telegramUserId: query.from.id,
+            referenceNumber: savedApplication.referenceNumber,
+            dateOfBirth: savedApplication.dateOfBirth,
+          })
+        } else if (query.data.startsWith('delete_')) {
+          if (!userDb.has(query.from.id)) {
+            userDb.set(query.from.id, { savedApplications: [] })
+          } else {
+            userDb.set(query.from.id, {
+              ...userDb.get(query.from.id)!,
+              savedApplications: savedApplications.filter(
+                (a) => a.referenceNumber !== referenceNumber,
+              ),
+            })
+            saveDb()
+            await goToScene(query.message, scenes.mainMenu)
+          }
+        }
+      }
+      break
+    }
   }
 })
 
 async function fetchApplicationStatus({
+  name,
   telegramUserId,
   referenceNumber,
   dateOfBirth,
 }: {
+  name: string
   telegramUserId: number
   referenceNumber: string
   dateOfBirth: Date
@@ -295,7 +536,7 @@ async function fetchApplicationStatus({
     if (rateLimitLeft < rateLimitInterval) {
       text =
         'Пожалуйста, подождите ' +
-        Math.round(rateLimitInterval - rateLimitLeft) +
+        Math.round((rateLimitInterval - rateLimitLeft) / 1000) +
         ' секунд перед следующим запросом'
       cacheMiss = false
     }
@@ -328,6 +569,21 @@ async function fetchApplicationStatus({
           updatedAt: Date.now(),
           status: status.status,
         })
+        const savedApplications = userDb.get(telegramUserId)?.savedApplications
+        if (
+          !savedApplications ||
+          !savedApplications.some((a) => a.referenceNumber === referenceNumber)
+        ) {
+          if (!userDb.has(telegramUserId)) {
+            userDb.set(telegramUserId, { savedApplications: [] })
+          }
+          userDb.get(telegramUserId)!.savedApplications.push({
+            referenceNumber,
+            dateOfBirth,
+            name: name,
+          })
+          saveDb()
+        }
       } else {
         text = status.error
       }
@@ -340,13 +596,26 @@ async function fetchApplicationStatus({
 
   const userState = userStates.get(telegramUserId)
   if (!userState || userState.state !== 'loading') return
+  const replyMarkup: TelegramBot.InlineKeyboardMarkup = {
+    inline_keyboard: [
+      [
+        {
+          text: 'Меню',
+          callback_data: 'main',
+        },
+      ],
+    ],
+  }
   if (userState.editMessageId) {
     await bot.editMessageText(text, {
       message_id: userState.editMessageId,
       chat_id: telegramUserId,
+      reply_markup: replyMarkup,
     })
   } else {
-    await bot.sendMessage(telegramUserId, text)
+    await bot.sendMessage(telegramUserId, text, {
+      reply_markup: replyMarkup,
+    })
   }
   userStates.delete(telegramUserId)
 }
